8d91906e8721354b99c1d66534bbeb03359a7f6f
Zumo <phornphatch@gmail.com>
2021-07-11 19:12:14 +0300

    init strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c45852ab41c97b4b4c592579e226f15f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5431fa0d-7b3b-4014-871f-79419feb1c5d'
+};
