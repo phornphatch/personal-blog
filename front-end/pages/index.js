88190302462db4b4f017057fcd960d110488d3e9
Zumo <phornphatch@gmail.com>
2021-07-27 15:52:57 +0300

    homepage

--- front-end/pages/index.js
+++ front-end/pages/index.js
@@ -1,14 +1,21 @@
 import Head from 'next/head'
 import axios from 'axios'
 import Image from 'next/image'
-import { Button, Row, Col, Layout, Menu } from 'antd'
+import { Button, Row, Col, Layout, Menu, Popover, BackTop } from 'antd'
 import banner from '../public/banner.png'
 import journey from '../public/journey.png'
 import contact2 from '../public/contact2.png'
-import { InstagramOutlined, GithubOutlined, YoutubeOutlined, SlackOutlined, FacebookOutlined, LinkedinOutlined } from '@ant-design/icons';
+import Ellipse from '../public/Ellipse.png'
+import Star from '../public/Star.png'
+import Rectangle from '../public/Rectangle.png'
+import Polygon from '../public/Polygon.png'
+import dot1 from '../public/dot1.png'
+import dot2 from '../public/dot2.png'
+import { InstagramOutlined, GithubOutlined, YoutubeOutlined, SlackOutlined, FacebookOutlined, LinkedinOutlined, SolutionOutlined, UpOutlined } from '@ant-design/icons';
 import { OverPack } from 'rc-scroll-anim';
 import TweenOne from 'rc-tween-one';
 import QueueAnim from 'rc-queue-anim';
+
 
 const { Header, Footer, Sider, Content } = Layout
 
@@ -46,11 +53,11 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
       <Layout>
         <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
           <div className="logo">ZUMO</div>
-          <Menu mode="horizontal" defaultSelectedKeys={['2']}>
-            <Menu.Item key="1">About Me</Menu.Item>
-            <Menu.Item key="2">Projects</Menu.Item>
-            <Menu.Item key="3">Gallery</Menu.Item>
-            <Menu.Item key="4">Contact Me</Menu.Item>
+          <Menu mode="horizontal" defaultSelectedKeys={['0']}>
+            <Menu.Item key="1"><a href="#about">About Me</a></Menu.Item>
+            <Menu.Item key="2"><a href="#project">Projects</a></Menu.Item>
+            <Menu.Item key="3"><a href="#gallery">Gallery</a></Menu.Item>
+            <Menu.Item key="4"><a href="#contact">Contact Me</a></Menu.Item>
           </Menu>
         </Header>
       </Layout>
@@ -75,11 +82,11 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
                   Start coding with all Polymer chemistry specialist skills and knowledge.
                 </p>
                 <Button type="primary" shape="round" size={'large'}>
-                  Contact Me
+                  <a href="#contact">Contact Me</a>
                 </Button>
               </Col>
               <Col span={10}>
-                <Image src={banner} alt="Picture of the author" />
+                <Image src={banner} alt="Picture of the author" className="banner-image" />
               </Col>
             </Row>
           </div>
@@ -90,8 +97,8 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
           <Col span={24}>
           <div className="marquee">
           <div className="text-box">
-            <h1> NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp; LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp;</h1>
-            <h1> NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp; LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp;</h1>
+            <h1>   NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp; LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp;</h1>
+            <h1>   NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp; LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp; NICE TO MEET YOU &nbsp; 😀 &nbsp;  LET'S CONVERT COFFEE INTO CODE &nbsp; ☕ &nbsp;</h1>
           </div>
           </div>
           </Col>
@@ -99,7 +106,7 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
        
 
         <>{/* Abouts */}</>
-        <div className="about-background">
+        <div className="about-background" id="about">
           <div className="about">
             <Row>
               <Col span={12} className="image">
@@ -116,16 +123,20 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
               <QueueAnim key="queue" delay={200} interval={500} duration={500} >
                 <div className="about-topic1" key="a"> This is not a resume ! </div>
                 <div className="about-topic2" key="b"> It's just.. </div>
-                <div className="about-topic2" key="c"> My Life Story. 🚀 </div>
-                <div className="container" key="d">
-                  {abouts.map((about) => {
-                    return (
-                      <a>
-                        <h3>{about.Title}</h3>
-                        <p>{about.Detail}</p>
+                <div className="about-topic" key="c"> My Life Story. 🚀 </div>
+                <div className="about-description" key="d">
+                  jkmfkvjmfldk'gjvbp'osdfgvjbgfdckbvmcf
+                  bvnmpfdigjbfkldbjfgkjbgdfjbzkbvjndroigkofigotidopihgopkblvcm
+                  x,ml;lfkg;flk;lk;ldoirgtopem.,cxvdklgjlisfd;gujfoidlhjfg
+                </div>
+                <div key="e">
+                  <Popover title="Download my resume here!">
+                    <Button type="primary" shape="round" size={'large'} >
+                      <a href="/Zumo's CV.pdf" download>
+                        <SolutionOutlined />
                       </a>
-                    )
-                  })}
+                    </Button>
+                  </Popover>
                 </div>
               </QueueAnim>
               </OverPack>
@@ -135,15 +146,25 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
         </div>
 
         <>{/* Projects */}</>
-        <div className="projects-background">
+        <div className="projects-background" id="project">
+        <div className="dot1"><Image src={dot1} width={350} height={250} /></div>
+        <div className="dot2"><Image src={dot2} width={380} height={270} /></div>
           <div className="projects">
             <Row>
               <Col span={12} className="project-detail">
-                <div className="topic">Coding Projects</div>
-                <div className="detail">the exmple of coding project from free code camp</div>
+                <p className="popout project-topic">
+                  <span>Coding </span> <br />
+                  <span> Projects </span>	
+                </p>
+                <div className="project-description">The exmple of coding project from free code camp</div>
+                <Button type="primary" shape="round" size={'large'} >
+                      SEE MORE
+                    </Button>
               </Col>
               <Col span={12}>
-                <div className="grid">
+              <OverPack>
+              <QueueAnim key="queue" delay={200} interval={500} duration={1000} >
+                <div className="grid" key="a">
                   {projects.map((project) => {
                     return (
                       <a href={project.Link} className="card">
@@ -153,14 +174,17 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
                     )
                   })}
                 </div>
+                </QueueAnim>
+                </OverPack>
               </Col>
             </Row>
+        
           </div>
+          
         </div>
 
-
         <>{/* Galleries */}</>
-        <div className="gallery-background">
+        <div className="gallery-background" id="gallery">
           <div className="gallery">
             <Row style={{ width: "100%" }}>
               <Col span={24}>
@@ -192,7 +216,7 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
         </div>
 
         <>{/* Contact */}</>
-        <div className="contact-background">
+        <div className="contact-background" id="contact">
           <div className="contact">
             <Row>
               <Col span={10} className="contact-image">
@@ -211,17 +235,20 @@ export default function Home({ projects, galleries, abouts }) { // {projects} = 
               </Col>
             </Row>
           </div>
+
+          <div className="ellipse"><Image src={Ellipse} width={260} height={200} /></div>
+          <div className="star" ><Image src={Star} width={200} height={200} /></div>
+          <div className="polygon"><Image src={Polygon} width={200} height={200} /></div>
+          <div className="rectangle"><Image src={Rectangle} width={200} height={200} /></div>
         </div>
+
+        <BackTop>
+          <div className="up"><UpOutlined /></div>
+        </BackTop>
 
       </main>
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Coded by ZUMO
-        </a>
       </footer>
     </div>
   )
